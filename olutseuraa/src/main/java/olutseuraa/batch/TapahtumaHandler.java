264f4c07b5a9b72cda0edc01554bc44abcf15229
Jukepoksi <crazunen@gmail.com>
2016-09-22 13:12:10 +0300

    lisätty tapahtuman haku kannasta id:llä ja osallistujamäärän päivitys.

--- olutseuraa/src/main/java/olutseuraa/batch/TapahtumaHandler.java
+++ olutseuraa/src/main/java/olutseuraa/batch/TapahtumaHandler.java
@@ -30,6 +30,35 @@ public class TapahtumaHandler {
 		}
 		System.out.println("-------------------");
 		
+		System.out.println("P‰ivitet‰‰n tapahtuma, jonka id on 1.");
+		//haetaan ensin tapahtuma, id:ll‰
+		Tapahtuma t1 = dao.haeTapahtuma(1);
+		//haetaan alkuper‰inen osallistujam‰‰r‰ ja lis‰t‰‰n siihen yksi.
+		int osallistujamaara = t1.getOsallistujat() + 1;
+		//p‰ivitet‰‰n osallistujam‰‰r‰ tietokantaan
+		dao.paivitaOsallistujat(t1.getId(), osallistujamaara);
+		//p‰ivitet‰‰n tapahtuma serverill‰, eli haetaan p‰ivitetty tieto tietokannasta
+		t1 = dao.haeTapahtuma(1);
+		System.out.println("Tapahtuman " + t1.getNimi() + " osallistujam‰‰r‰ on nyt: " + t1.getOsallistujat());
+		System.out.println("-------------------");
+		
+		System.out.println("Haetaan tapahtumat");
+		System.out.println("-------------------");
+		tapahtumat = dao.haeKaikki();
+		for(Tapahtuma t : tapahtumat) {
+			System.out.println("id: " + t.getId());
+			System.out.println("nimi: " + t.getNimi());
+			System.out.println("pvm: " + t.getPvm());
+			System.out.println("aika: " + t.getAika());
+			System.out.println("paikka: " + t.getPaikka());
+			System.out.println("teema: " + t.getTeema());
+			System.out.println("osallistujat: " + t.getOsallistujat());
+			System.out.println("is‰nt‰: " + t.getIsanta());
+			System.out.println("kuvaus: " + t.getKuvaus());
+		}
+		System.out.println("-------------------");
+		
+		
 		context.close();
 	}
 
--- olutseuraa/src/main/java/olutseuraa/dao/TapahtumaDAOSpringJdbcImpl.java
+++ olutseuraa/src/main/java/olutseuraa/dao/TapahtumaDAOSpringJdbcImpl.java
@@ -25,12 +25,28 @@ public class TapahtumaDAOSpringJdbcImpl implements TapahtumaDAO {
 		
 		jdbcTemplate.update(sql , parametrit);
 	}
-
+	
 	public List<Tapahtuma> haeKaikki() {
 		String sql  = "SELECT id, nimi, pvm, aika, paikka, teema, osallistujat, isanta, kuvaus FROM Tapahtuma";
 		RowMapper<Tapahtuma> mapper = new TapahtumaRowMapper();
 		List<Tapahtuma> tapahtumat = jdbcTemplate.query(sql,mapper);
 		return tapahtumat;
+	}
+
+	public void paivitaOsallistujat(int id, int osallistujat) {
+		String sql = "UPDATE Tapahtuma SET osallistujat=? WHERE id=?";
+		Object[] parametrit = new Object[] { osallistujat, id };
+		
+		jdbcTemplate.update(sql , parametrit);
+	}
+
+	public Tapahtuma haeTapahtuma(int id) {
+		String sql = "SELECT id, nimi, pvm, aika, paikka, teema, osallistujat, isanta, kuvaus FROM Tapahtuma WHERE id=?";
+		Object[] parametrit = new Object[] { id };
+		RowMapper<Tapahtuma> mapper = new TapahtumaRowMapper();
+		
+		Tapahtuma t = jdbcTemplate.queryForObject(sql, parametrit, mapper);
+		return t;
 	}
 
 }

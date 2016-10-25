57a389edc6446b57706be20f56b0b3769c4e8e1d
Hämäläinen Sebastian <a1500919@myy.haaga-helia.fi>
2016-10-25 11:24:55 +0300

    logini lisätty adminille

--- olutseuraa/database/update_tables.sql
+++ olutseuraa/database/update_tables.sql
@@ -5,3 +5,7 @@ WHERE id = 2;
 UPDATE Tapahtuma
 SET pvm = "12.10.2016"
 WHERE id = 1;
+
+UPDATE webuser2
+SET password_encrypted = "8e3c77a2c6d4f1025650f7e4a33802d66194661e87ab7e68c9285a8362c1a85c110cc1ca99fea10a"
+WHERE id = 1;

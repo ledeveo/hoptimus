f4159294eeec6d6a9229021e08177b9ec37ac424
Heikkinen Juho <a1500925@myy.haaga-helia.fi>
2016-11-24 11:36:38 +0200

    listan duplikaattinäyttö korjattu

--- olutseuraa/src/main/java/fi/hoptimus/olutseuraa/helper/Helpperi.java
+++ olutseuraa/src/main/java/fi/hoptimus/olutseuraa/helper/Helpperi.java
@@ -1,9 +1,7 @@
 package fi.hoptimus.olutseuraa.helper;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import fi.hoptimus.olutseuraa.bean.Henkilo;
 import fi.hoptimus.olutseuraa.bean.Tapahtuma;
 
 public class Helpperi {
@@ -24,13 +22,13 @@ public class Helpperi {
 		
 		//lisää uuteen listaan tapahtumat ilman duplikaatteja
 		for(int i = 0; i < tapahtumat.size(); i++) {
-			for(int j = 1; j < tapahtumat2.size(); j++) {
+			for(int j = 0; j < tapahtumat2.size(); j++) {
 				
-				if(j < tapahtumat2.size() && i < tapahtumat.size()) {
+				if(j <= tapahtumat2.size() && i <= tapahtumat.size()) {
 					//jos id sama, poista jälkimmäisempi uudesta listasta
 					if(tapahtumat.get(i).getId() == tapahtumat2.get(j).getId()) {
 						
-						//jos olemassa uudessa listassa
+						//jos olemassa uudessa listassa ja ei sama indeksi
 						if(tapahtumat2.get(j) != null && i != j) {
 							
 							//poista duplikaatti
@@ -41,6 +39,34 @@ public class Helpperi {
 				
 			}
 		}
+		//jostain syystä tässä kohtaa on vielä kaksi samaa tapahtumaa, tehrääs luuppi uurelleen
+		List<Tapahtuma> tapahtumat3 = tapahtumat2;
+		
+		//lisää uuteen listaan tapahtumat ilman duplikaatteja
+		for(int i = 0; i < tapahtumat2.size(); i++) {
+			for(int j = 0; j < tapahtumat3.size(); j++) {
+				
+				if(j <= tapahtumat3.size() && i <= tapahtumat2.size()) {
+					//jos id sama, poista jälkimmäisempi uudesta listasta
+					if(tapahtumat2.get(i).getId() == tapahtumat3.get(j).getId()) {
+						
+						//jos olemassa uudessa listassa ja ei sama indeksi
+						if(tapahtumat3.get(j) != null && i != j) {
+							
+							//poista duplikaatti
+							tapahtumat3.remove(j);
+						}
+					}
+				}
+				
+			}
+		}
+		/*//DEBUG
+		 * System.out.println("tapahtumat2 sisältö:");
+		for(int i = 0; i < tapahtumat.size(); i++) {
+			System.out.println("tapahtuma id: " + tapahtumat.get(i).getId() + ", osallistujia: " + tapahtumat.get(i).getOsallistujamaara());
+		}
+		*/
 		return tapahtumat2;
 	}
 
--- olutseuraa/src/test/java/fi/hoptimus/olutseuraa/tests/HelperTesteri.java
+++ olutseuraa/src/test/java/fi/hoptimus/olutseuraa/tests/HelperTesteri.java
@@ -23,14 +23,26 @@ public class HelperTesteri {
 		Tapahtuma t1 = new TapahtumaImpl();
 		t1.setId(1);
 		Tapahtuma t2 = new TapahtumaImpl();
-		t2.setId(2);
+		t2.setId(1);
 		Tapahtuma t3 = new TapahtumaImpl();
 		t3.setId(1);
+		Tapahtuma t4 = new TapahtumaImpl();
+		t4.setId(2);
+		Tapahtuma t5 = new TapahtumaImpl();
+		t5.setId(2);
+		Tapahtuma t6 = new TapahtumaImpl();
+		t5.setId(3);
+		Tapahtuma t7 = new TapahtumaImpl();
+		t5.setId(4);
 		
 		//lisää listaan tapahtumat
 		tapahtumat.add(t1);
 		tapahtumat.add(t2);
 		tapahtumat.add(t3);
+		tapahtumat.add(t4);
+		tapahtumat.add(t5);
+		tapahtumat.add(t6);
+		tapahtumat.add(t7);
 		
 
 		System.out.println("ennen duplikaattien poistoa lista: " + tapahtumat);
@@ -39,14 +51,27 @@ public class HelperTesteri {
 		//poista listasta duplikaatit
 		tapahtumat = Helpperi.PoistaListastaDuplikaatit(tapahtumat);
 
+		
 		System.out.println("duplikaattien poiston jälkeen lista: " + tapahtumat);
-		System.out.println(tapahtumat.get(0).getId());
+		System.out.println("tapahtuman 1. id: " + tapahtumat.get(0).getId());
+		System.out.println("tapahtuman 2. id: " + tapahtumat.get(1).getId());
+		System.out.println("tapahtuman 3. id: " + tapahtumat.get(2).getId());
+		System.out.println("tapahtuman 4. id: " + tapahtumat.get(3).getId());
+		
+		System.out.println("tapahtumaan 1. osallistuu: " + tapahtumat.get(0).getOsallistujamaara());
+		System.out.println("tapahtumaan 2. osallistuu: " + tapahtumat.get(1).getOsallistujamaara());
+		System.out.println("tapahtumaan 3. osallistuu: " + tapahtumat.get(2).getOsallistujamaara());
+		System.out.println("tapahtumaan 4. osallistuu: " + tapahtumat.get(3).getOsallistujamaara());
 		
 		//testaa tapahtumat
 		assertEquals(tapahtumat.get(0).getId(), 1);
 		assertEquals(tapahtumat.get(1).getId(), 2);
-		assertEquals(tapahtumat.get(0).getOsallistujamaara(), 2);
+		assertEquals(tapahtumat.get(2).getId(), 4);
+		assertEquals(tapahtumat.get(3).getId(), 0);
+		assertEquals(tapahtumat.get(0).getOsallistujamaara(), 3);
 		assertEquals(tapahtumat.get(1).getOsallistujamaara(), 1);
+		assertEquals(tapahtumat.get(2).getOsallistujamaara(), 1);
+		assertEquals(tapahtumat.get(3).getOsallistujamaara(), 2);
 		
 		
 	}

78c1304694dd1c91666efdb514eb63b7c812a3d0
Joonas Heikkinen <solarshadow.jh@gmail.com>
2016-12-06 16:57:22 +0200

    Palautteiden hakemista

--- olutseuraa/src/main/webapp/resources/js/ajaxkikkailu.js
+++ olutseuraa/src/main/webapp/resources/js/ajaxkikkailu.js
@@ -3,121 +3,152 @@
  */
 
 function Lisaa(nappula, tID) {
-	
+
 	$(nappula).hide();
 	suurennaKuva();
 	// alert("lisaa");
-    $.ajax({
-        url: 'LisaaLiittyminen',
-        type:'POST',
-        data:
-        {
-        	tapahtumaId: tID
-        },
-        success: function(msg)
-        {
-            // alert('Lisäys onnistui!');
-        	// tän vois tehrä paremminki. esim.
-        	// muuntaa elementissä numeron kokoo vain jotenki?
-        	setTimeout(function() {location.reload()}, 800);
-        }               
-    });
+	$.ajax({
+		url : 'LisaaLiittyminen',
+		type : 'POST',
+		data : {
+			tapahtumaId : tID
+		},
+		success : function(msg) {
+			// alert('Lisäys onnistui!');
+			// tän vois tehrä paremminki. esim.
+			// muuntaa elementissä numeron kokoo vain jotenki?
+			setTimeout(function() {
+				location.reload()
+			}, 800);
+		}
+	});
 }
 
-function Poista(nappula, tID) 
-{
+function Poista(nappula, tID) {
 	$(nappula).hide();
 	pienennaKuva();
 	// alert("poista");
-    $.ajax({
-        url: 'PoistaLiittyminen',
-        type:'POST',
-        data:
-        {
-        	tapahtumaId: tID
-        },
-        success: function(msg)
-        {
-            // alert('Poisto onnistui!');
-        	setTimeout(function() {location.reload()}, 800);
-        }               
-    });
+	$.ajax({
+		url : 'PoistaLiittyminen',
+		type : 'POST',
+		data : {
+			tapahtumaId : tID
+		},
+		success : function(msg) {
+			// alert('Poisto onnistui!');
+			setTimeout(function() {
+				location.reload()
+			}, 800);
+		}
+	});
 }
 
+$("#palauteform")
+		.submit(
+				function(event) {
 
-$("#palauteform").submit(function(event) {
+					/* stop form from submitting normally */
+					event.preventDefault();
+					var originalState = $("#contactbtndiv").clone();
 
-    /* stop form from submitting normally */
-    event.preventDefault();
-    var originalState = $("#contactbtndiv").clone();
+					var postData = $("#palauteform").serialize();
+					$
+							.ajax({
+								type : "post",
+								url : "palautetta",
+								data : postData,
+								contentType : "application/x-www-form-urlencoded",
+								accepts : "text/plain",
+								success : function(responseData, textStatus,
+										jqXHR) {
+									console.log(responseData);
+									$('#contact').foundation('close');
+									$("#contactbtndiv")
+											.hide()
+											.html(
+													"<button class='button center small success'>"
+															+ "Kiitos palautteesta! <i class='fi-like small' ></i></button>")
+											.fadeIn(1500)
+											.delay(1500)
+											.queue(
+													function() {
+														$("#contactbtndiv")
+																.replaceWith(
+																		originalState);
+													});
 
-    var postData = $("#palauteform").serialize();
-    $.ajax({
-          type: "post",
-          url: "palautetta",
-          data: postData,
-          contentType: "application/x-www-form-urlencoded",
-          accepts: "text/plain",
-          success: function(responseData, textStatus, jqXHR) {
-        	  console.log(responseData);
-        	  $('#contact').foundation('close');
-        	  $("#contactbtndiv").hide().html("<button class='button center small success'>" +
-        	  		"Kiitos palautteesta! <i class='fi-like small' ></i></button>").fadeIn(1500).delay(1500).queue(function(){
-        	        	  $("#contactbtndiv").replaceWith(originalState);
-        	  });        	  
-        	  		      			        	  
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-              console.log(errorThrown);
-              $('#contact').foundation('close');
-              $("#contactbtndiv").hide().html("<button class='button center small alert'>" +
-  	  		"Palaute ei mennyt perille. Yritä myöhemmin uudestaan. <i class='fi-alert small' ></i></button>").fadeIn(1500).delay(2500).queue(function(){
-  	        	  $("#contactbtndiv").replaceWith(originalState);
-  	  });  
-          }
-      })
-})
+								},
+								error : function(jqXHR, textStatus, errorThrown) {
+									console.log(errorThrown);
+									$('#contact').foundation('close');
+									$("#contactbtndiv")
+											.hide()
+											.html(
+													"<button class='button center small alert'>"
+															+ "Palaute ei mennyt perille. Yritä myöhemmin uudestaan. <i class='fi-alert small' ></i></button>")
+											.fadeIn(1500)
+											.delay(2500)
+											.queue(
+													function() {
+														$("#contactbtndiv")
+																.replaceWith(
+																		originalState);
+													});
+								}
+							})
+				})
 
-$("#palauteAjax").click(function(){
+//$(function() {
+//	hae("viimeisetViisi");
+//})
+
+$("#kaikkip, #five").click(function() {
+
+	alert(this.id); 
+	var url;
+	if(this.id == "five"){
+		url = "viimeisetViisi";
+	}else{
+		url = "kaikkiPalautteet";
+	}
 	
 	
-	$("#five").toggle();
-	$("#palauteAjax").toggle();
 
+	$.ajax({
+				type : "get",
+				url : url,
+				dataType : "json",
+				success : function(responseData, textStatus, jqXHR) {
+
+						$("#kaikkip").toggle();
+						$("#five").toggle();
+						$("#listitem").empty();
+				
+					$
+							.each(
+									responseData,
+									function(index, palaute) {
+										
+									$("#listitem").append("<li class='accordion-item' data-accordion-item >" 
+										+	"<a href='#'class='accordion-title'><h5>" +palaute.aikaleima + "</h5></a>"
+										+	"<div class='accordion-content' data-tab-content><table>"
+										+	"<tbody><tr><th>Palautteen antaja</th></tr>"
+										+	"<tr><td>" +palaute.palautteenAntaja+"</td></tr><tr><th>Palaute</th></tr>"
+										+		"<tr><td>"+palaute.palaute+ "</td></tr><tr><th>Sähköposti</th></tr>"
+										+	"<tr><td>"+palaute.sposti + "</td></tr></tbody></table></div></li>");
+
+									
+									})
+									Foundation.reInit('accordion');
+				},
+				error : function(jqXHR, textStatus, errorThrown) {
+					console.log("Error" + errorThrown);
+
+				}
+
+				
+			})
+			
+			
 		
-    $.ajax({
-          type: "get",
-          url: "kaikkiPalautteet",
-          dataType: "json",
-          success: function(responseData, textStatus, jqXHR) {
-              
-        	    
-        	  
-        	  $.each(responseData, function( index, palaute ) {
-        		  
-        		  $( "#ajaxdiv" ).append( "<ul class='accordion center' data-accordion data-allow-all-closed='true'>");
-        		  $("#ajaxdiv").append("<li class='accordion-item' data-accordion-item><a href='#' class='accordion-title'><h5>Kakka</h5></a>");
-        		  $("#ajaxdiv").append ("<div class='accordion-content' data-tab-content>");
-        		  $( "#ajaxdiv" ).append( "<table><tbody><tr>");	
-        		  $( "#ajaxdiv" ).append( "<th>Palautteen antaja </th></tr>" );
-        		  $( "#ajaxdiv" ).append( "<tr><td>" + palaute.palautteenAntaja + "</td></tr>" );
-        		  $( "#ajaxdiv" ).append( "<tr><th>Palaute </th></tr>" );
-        		  $( "#ajaxdiv" ).append( "<tr><td>" + palaute.palaute + "</td></tr>" );
-        		  $( "#ajaxdiv" ).append( "<tr><th>Sähköposti </th></tr>" );
-        		  $( "#ajaxdiv" ).append( "<tr><td>" + palaute.sposti + "</td></tr>" );
-        		  $( "#ajaxdiv" ).append( "</tbody></table></div></li></ul>");
-        		 
-        		  
-        		})     
-        
-        	  	    
-        	  
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-              console.log("Error" + errorThrown);
-          
-          }
-      })
 })
-
-
